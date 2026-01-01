7611ed2ae06bd450f8a9db47c3e587ca9585df1d
BRINGMETHANOS\oyeet <officialtalha00@gmail.com>
2026-01-01 05:18:44 +0500

    - add global error handling middleware - used pino logger and pino logger http to log http req and res and errors - add AppError class for custom error handling - some old code refactoring

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,11 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+
+import {pinoHttp} from 'pino-http';
+import logger from './shared/utils/logger.js';
+import { globalErrorHandler } from './shared/middlewares/globalErrorHandler.js';
+import { AppError } from './shared/errors/AppError.js';
+
 import { ExpressAuth } from "@auth/express";
 import { authConfig } from './features/auth/config.js';
 
@@ -9,6 +15,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  pinoHttp({
+    logger,
+    // Custom serializers to clean up the output
+    serializers: {
+      req: (req: any) => ({
+        method: req.method,
+        url: req.url.split('?')[0],
+      }),
+      res: (res: any) => ({
+        statusCode: res.statusCode,
+      }),
+    },
+    wrapSerializers: true,
+  })
+);
+
+app.use(express.json());
+
 //Auth Setup
 // Trust Proxy is crucial for OAuth callbacks to correctly identify 'https' vs 'http'
 app.set('trust proxy', true);
@@ -17,6 +42,21 @@ app.set('trust proxy', true);
 app.use('/auth', ExpressAuth(authConfig));
 
 //Express Routes
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to the 10PShine Notes App Backend!',
+    data: {
+      service: '10pShine-backend',
+      version: '1.0.0', 
+      environment: process.env.NODE_ENV || 'development',
+      serverTime: new Date().toISOString(),
+      uptime: process.uptime()
+    }
+  });
+});
+
 app.get('/health', (req: Request, res: Response) => {
     res.status(200).json({ status: 'OK' });
 });
@@ -29,5 +69,13 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Internal Server Error' });
 });
+
+
+app.all(/(.*)/, (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+
+app.use(globalErrorHandler);
 
 export default app;
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,18 +1,19 @@
 import mongoose from 'mongoose';
+import logger from '../shared/utils/logger.js';
+import { AppError } from '../shared/errors/AppError.js';
 
 const MONGO_URI = process.env.DB_URI;
 
 if (!MONGO_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+  throw new AppError('Invalid/Missing environment variable: "DB_URI"', 500);
 }
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGO_URI);
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(MONGO_URI);
+    logger.info('MongoDB connected');
+  } catch (error) {
+    logger.error(error, 'MongoDB connection error');
+    process.exit(1);
+  }
 };
-
